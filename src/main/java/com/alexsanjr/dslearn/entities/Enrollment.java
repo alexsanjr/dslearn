97e5aad888ad65e37228d8af32474e9596bf30fd
Alex Sandro <alex.sandro.1199@gmail.com>
2024-04-08 15:33:14 -0300

    Lesson, Content and Task

--- src/main/java/com/alexsanjr/dslearn/entities/Enrollment.java
+++ src/main/java/com/alexsanjr/dslearn/entities/Enrollment.java
@@ -4,9 +4,6 @@ import com.alexsanjr.dslearn.entities.pk.EnrollmentPK;
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 import java.time.Instant;
