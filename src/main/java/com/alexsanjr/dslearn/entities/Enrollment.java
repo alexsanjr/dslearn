8c031c0ade50b3f0ef0f962cf92b526009e6d949
Alex Sandro <alex.sandro.1199@gmail.com>
2024-04-09 16:24:44 -0300

    Topic, Reply

--- src/main/java/com/alexsanjr/dslearn/entities/Enrollment.java
+++ src/main/java/com/alexsanjr/dslearn/entities/Enrollment.java
@@ -4,7 +4,6 @@ import com.alexsanjr.dslearn.entities.pk.EnrollmentPK;
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
